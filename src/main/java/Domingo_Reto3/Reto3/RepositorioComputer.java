5cfc14a472acb1135884614fdda84dc696f8ad86
isaac2021 <isaac.gonzalez.mt@correo.usa.edu.co>
2021-11-02 00:37:15 -0500

    Primer commit

---
+++ src/main/java/Domingo_Reto3/Reto3/RepositorioComputer.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Domingo_Reto3.Reto3;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Repository
+public class RepositorioComputer {
+    
+    @Autowired
+    private InterfaceComputer crud;
+
+    public List<Computer> getAll(){
+        return (List<Computer>) crud.findAll();
+    }
+
+    public Optional<Computer> getBike(int id){
+        return crud.findById(id);
+    }
+
+    public Computer save(Computer computer){
+        return crud.save(computer);
+    }
+    public void delete(Computer computer){
+        crud.delete(computer);
+    }
+
+    Optional<Computer> getComputer(int computerId) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
